dcb31d4e48a9a538c1fa64b55497c8cda7da531c
marin <clement.marin@laplateforme.io>
2023-01-18 18:49:22 +0100

    [FIXED] (BUG KNOWN) doesnt show accurate time line in musics longer than 250 second (BUG KNOWN) [ADD] current time and max time timer, [FIXED] Minor bugs

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ import os
 p = False
 current_pos = 0
 
-
 root = tkinter.Tk()
-#root.resizable(False, False)
-root.title("Music Player")
-root.geometry("256x325")
+root.resizable(False, False)
+root.title("IPOD")
+root.geometry("256x425")
 
 var = tkinter.StringVar()
 var.set("Select the song to play")
+
+
+current_time_max_ui = tkinter.StringVar()
+current_time_ui = tkinter.StringVar()
 
 os.chdir(askdirectory())
 songlist = os.listdir()
@@ -24,21 +27,21 @@ playing = tkinter.Listbox(root, font="Roboto,12", width=28, bg="black", fg="whit
 for song in songlist:
     playing.insert(0, song)
 
-
 mixer.init()
 
 
 def play():
-    print(mixer.Sound(playing.get(tkinter.ACTIVE)).get_length())
     global p
+    current_max = mixer.Sound(playing.get(tkinter.ACTIVE)).get_length()
     reset_progressbar()
-    update_progressbar()
     mixer.music.load(playing.get(tkinter.ACTIVE))
     name = playing.get(tkinter.ACTIVE)
     var.set(f"{name[:20]}..." if len(name) > 22 else name)
     mixer.music.play()
     p = False
     pause_button.config(text="Pause")
+    current_time_max_ui.set(str(convert(current_max)))
+    progressbar.config(maximum=current_max)
 
 
 def pause():
@@ -50,7 +53,6 @@ def pause():
     else:
         mixer.music.unpause()
         p = False
-        update_progressbar()
         pause_button.config(text="Pause")
 
 
@@ -89,20 +91,26 @@ def next_song():
 
 
 def update_progressbar():
-    global current_pos
-    progressbar.config(value=current_pos)
-    current_pos += 1
-    print(current_pos)
-    if mixer.music.get_busy() and not p:
+    if not p and mixer.music.get_busy():
+        global current_pos
+        progressbar.config(value=current_pos)
+        current_pos += 1
+        current_time_ui.set(str(convert(current_pos)))
+        root.after(1000, update_progressbar)
+    else:
         root.after(1000, update_progressbar)
 
 
 def on_progressbar_click(event):
-    global current_pos
+    global current_pos, p
     new_pos = int(event.x * progressbar['maximum'] / progressbar.winfo_width())
     current_pos = new_pos
     mixer.music.rewind()
     mixer.music.set_pos(new_pos)
+    if p:
+        mixer.music.unpause()
+        p = False
+        pause_button.config(text="Pause")
 
 
 def reset_progressbar():
@@ -111,34 +119,53 @@ def reset_progressbar():
     progressbar.config(value=current_pos)
 
 
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+
+    return "%02d:%02d" % (minutes, seconds)
+
+
+current_t = tkinter.Label(root, font="Roboto,12", textvariable=current_time_ui)
+current_t.grid(row=2, column=0)
+
+max_t = tkinter.Label(root, font="Roboto,12", textvariable=current_time_max_ui)
+max_t.grid(row=2, column=2)
+
 text = tkinter.Label(root, font="Roboto,12", textvariable=var)
-text.grid(row=0, columnspan=3)
+text.grid(row=3, columnspan=3)
 
-playing.grid(columnspan=3)
 
-play_button = tkinter.Button(root, width=7, height=1,font="Roboto", text="Play", command=play)
-play_button.grid(row=3, column=1)
+text = tkinter.Label(root, font="Roboto,12", textvariable=var)
+text.grid(row=3, columnspan=3)
+
+playing.grid(columnspan=3, row=0)
+
+play_button = tkinter.Button(root, width=7, height=1, font="Roboto", text="Play", command=play)
+play_button.grid(row=4, column=1)
 
 pause_button = tkinter.Button(root, width=7, height=1, font="Roboto", text="Pause", command=pause, fg="black")
-pause_button.grid(row=4, column=1)
+pause_button.grid(row=5, column=1)
 
 sound_label = tkinter.Label(root, font="Roboto,12", text="Volume:")
-sound_label.grid(row=5, column=0)
+sound_label.grid(row=6, column=0)
 
 volume = tkinter.Scale(root, from_=0, to=100, orient="horizontal", command=change_volume)
 volume.set(100)
-volume.grid(row=5, column=1)
+volume.grid(row=6, column=1)
 
 next_button = tkinter.Button(root, width=7, height=1, font="Roboto", text="Next", command=next_song)
-next_button.grid(row=4, column=2)
+next_button.grid(row=5, column=2)
 
 previous_button = tkinter.Button(root, width=7, height=1, font="Roboto", text="Previous", command=previous_song)
-previous_button.grid(row=4, column=0)
+previous_button.grid(row=5, column=0)
 
-progressbar = ttk.Progressbar(root, maximum=mixer.Sound(playing.get(tkinter.ACTIVE)).get_length(), length=250)
-progressbar.grid(row=2, columnspan=3)
+progressbar = ttk.Progressbar(root, length=250)
+progressbar.grid(row=1, columnspan=3)
 progressbar.bind("<Button-1>", on_progressbar_click)
 
-if mixer.music.get_busy() and not p:
-    update_progressbar()
+
+update_progressbar()
 root.mainloop()
